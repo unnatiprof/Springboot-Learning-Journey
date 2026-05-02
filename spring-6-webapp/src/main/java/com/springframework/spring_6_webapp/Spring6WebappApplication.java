7c8c5b674e284d0696c28770c256f67a0c0cca5c
unnatiprof <unnatiprofessional@gmail.com>
2026-05-02 18:46:32 +0530

    Added the initial project folder

---
+++ spring-6-webapp/src/main/java/com/springframework/spring_6_webapp/Spring6WebappApplication.java
@@ -0,0 +1,14 @@
+package com.springframework.spring_6_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6WebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6WebappApplication.class, args);
+		System.out.println("Unnati");
+	}
+
+}
---
+++ spring-6-webapp/src/main/java/com/springframework/spring_6_webapp/domain/Author.java
@@ -0,0 +1,53 @@
+package com.springframework.spring_6_webapp.domain;
+
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+@Entity
+public class Author {
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String firstName;
+	private String lastName;
+	
+	@ManyToMany(mappedBy = "authors")
+	private Set<Book> books;
+	
+	public Set<Book> getBooks() {
+		return books;
+	}
+	public void setBooks(Set<Book> books) {
+		this.books = books;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+}
