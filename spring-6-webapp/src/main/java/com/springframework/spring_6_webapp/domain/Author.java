f4bfa534761f018a3d96ecd70456ae5e2ca856fe
unnatiprof <unnatiprofessional@gmail.com>
2026-05-03 14:38:43 +0530

    Added publisher Repository in the project.

--- spring-6-webapp/src/main/java/com/springframework/spring_6_webapp/domain/Author.java
+++ spring-6-webapp/src/main/java/com/springframework/spring_6_webapp/domain/Author.java
@@ -1,7 +1,6 @@
 package com.springframework.spring_6_webapp.domain;
 
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 
 import jakarta.persistence.Entity;
@@ -13,68 +12,41 @@ import jakarta.persistence.ManyToMany;
 @Entity
 public class Author {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long id;
-	private String firstName;// column in DB
-	private String lastName;// column in DB
-	
-	@ManyToMany(mappedBy = "authors")// one book can have many authors and many authors can have one book
-	// mapped by authors means book owns the relationship(main side)
-	private Set<Book> books = new HashSet<>();
-	
-	public Set<Book> getBooks() {
-		return books;
-	}
-	public void setBooks(Set<Book> books) {
-		this.books = books;
-	}
-	
-	
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	
-	
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	
-	
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	
-	
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (o == null)
-			return false;
-		if (getClass() != o.getClass())
-			return false;
-		Author other = (Author) o;
-		return id == other.id;
-	}
-	
-	
-	@Override
-	public String toString() {
-		return "Author [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", books=" + books + "]";
-	}
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
 }
--- spring-6-webapp/src/main/java/com/springframework/spring_6_webapp/domain/Book.java
+++ spring-6-webapp/src/main/java/com/springframework/spring_6_webapp/domain/Book.java
@@ -1,88 +1,65 @@
 package com.springframework.spring_6_webapp.domain;
 
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 
-@Entity //Create a table named book in DB
+@Entity
 public class Book {
 
-	@Id // primary key
-	@GeneratedValue(strategy = GenerationType.AUTO) // GeneratedValue -> auto increment
-	private Long id;
-	private String title;
-	private String isbn;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
-	
-	//owner side
-	@ManyToMany
-	// JoinTable joins tables & JoinColumn joins columns
-	@JoinTable(name = "author_book", joinColumns = @JoinColumn(name ="book_id"),
-	inverseJoinColumns =@JoinColumn(name = "author_id"))
-	private Set<Author> authors = new HashSet<>();
-	
-	
-	public Set<Author> getAuthors() {
-		return authors;
-	}
-	public void setAuthors(Set<Author> authors) {
-		this.authors = authors;
-	}
-	
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	
-	
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	
-	
-	public String getIsbn() {
-		return isbn;
-	}
-	public void setIsbn(String isbn) {
-		this.isbn = isbn;
-	}
-	
-	
-	
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Book other = (Book) obj;
-		return Objects.equals(id, other.id);
-		
-		
-	}
-	
-	@Override
-	public String toString() {
-		return "Book [id=" + id + ", title=" + title + ", isbn=" + isbn + ", authors=" + authors + "]";
-	}
+    private String title;
+    private String isbn;
+    
+    
+    @ManyToOne
+    private Publisher publisher;
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
+    }
+
+    @ManyToMany
+    private Set<Author> authors = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
 }
